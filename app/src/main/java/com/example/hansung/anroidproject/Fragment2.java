02f598205d7815df243fc8689426056786693610
Bohyunnn <kbh236@naver.com>
2017-12-03 17:48:13 +0900

    material spinner 변경

--- app/src/main/java/com/example/hansung/anroidproject/Fragment2.java
+++ app/src/main/java/com/example/hansung/anroidproject/Fragment2.java
@@ -13,6 +13,10 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.jaredrummler.materialspinner.MaterialSpinner;
+
+import java.util.ArrayList;
+
 public class Fragment2 extends Fragment {
     private ViewGroup rootView;
     ArrayAdapter<CharSequence> adspin1, adspin2;
@@ -31,18 +35,19 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         rootView = (ViewGroup) inflater.inflate(R.layout.activity_fragment2, container, false);
 
-        final Spinner spin1 = (Spinner) rootView.findViewById(R.id.spinner);
-        final Spinner spin2 = (Spinner) rootView.findViewById(R.id.spinner2);
+        final MaterialSpinner spin1 = (MaterialSpinner) rootView.findViewById(R.id.spinner);
+        final MaterialSpinner spin2 = (MaterialSpinner) rootView.findViewById(R.id.spinner2);
+
         Button btn_refresh = (Button) rootView.findViewById(R.id.btn_refresh);
         //final=> spin2가 함수안에서 사용되기 때문에 코딩전체로 선언.
 
-        adspin1 = ArrayAdapter.createFromResource(getContext(), R.array.spinner_do, android.R.layout.simple_spinner_dropdown_item);
+        adspin1 = ArrayAdapter.createFromResource(getContext(), R.array.spinner_do, R.layout.custom_simple_dropdown_item_line1);
 
         spin1.setAdapter(adspin1);
 
-        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spin1.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
                 //spin1 누르면 발생하는 이벤트
                 //서울인 경우
                 if (adspin1.getItem(position).equals("서울")) {
@@ -50,66 +55,51 @@ public class Fragment2 extends Fragment {
                     adspin2 = ArrayAdapter.createFromResource(getContext(), R.array.spinner_do_seoul, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spin2.setAdapter(adspin2);
-                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    spin2.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
                         @Override
-                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
                             choice_se = adspin2.getItem(position).toString();
                         }
 
-                        @Override
-                        public void onNothingSelected(AdapterView<?> parent) {
-                            //아무것도 선택되지 않은 경우
-                        }
                     });
                 }
                 //인천인 경우
-                else if(adspin1.getItem(position).equals("인천")){
+                else if (adspin1.getItem(position).equals("인천")) {
                     choice_do = "인천";
                     adspin2 = ArrayAdapter.createFromResource(getContext(), R.array.spinner_do_incheon, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spin2.setAdapter(adspin2);
-                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    spin2.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
                         @Override
-                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
                             choice_se = adspin2.getItem(position).toString();
-                        }
-
-                        @Override
-                        public void onNothingSelected(AdapterView<?> parent) {
-                            //아무것도 선택되지 않은 경우
                         }
                     });
                 }
                 //경기도인 경우
-                else if(adspin1.getItem(position).equals("경기도")){
+                else if (adspin1.getItem(position).equals("경기도")) {
                     choice_do = "경기도";
                     adspin2 = ArrayAdapter.createFromResource(getContext(), R.array.spinner_do_gyeonggi, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spin2.setAdapter(adspin2);
-                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    spin2.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
                         @Override
-                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
                             choice_se = adspin2.getItem(position).toString();
                         }
 
-                        @Override
-                        public void onNothingSelected(AdapterView<?> parent) {
-                            //아무것도 선택되지 않은 경우
-                        }
                     });
                 }
             }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                //아무것도 선택되지 않은 경우
-            }
+
         });
 
         btn_refresh.setOnClickListener(new View.OnClickListener() {//버튼 클릭시 이벤트입니다.
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), choice_do + "=" + choice_se, Toast.LENGTH_SHORT).show();
-        }});
+                Snackbar.make(view, " " + choice_do+" "+choice_se+" 검색합니다.", Snackbar.LENGTH_LONG).show();
+            }
+        });
 
         return rootView;
 
