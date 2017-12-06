2f6692f35195755c89f66bc0b37c1762312eb0f6
Bohyunnn <kbh236@naver.com>
2017-12-06 20:38:24 +0900

    ServiceContact, ServiceInfo 구현

--- app/src/main/java/com/example/hansung/anroidproject/ServiceContact.java
+++ app/src/main/java/com/example/hansung/anroidproject/ServiceContact.java
@@ -1,15 +1,34 @@
 package com.example.hansung.anroidproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ServiceContact extends AppCompatActivity {
+    private Button button;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_contact);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageView=(ImageView)findViewById(R.id.imageView3);
+        button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url ="http://13.124.59.137:8080";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 
     //ActionBar's Back Button
